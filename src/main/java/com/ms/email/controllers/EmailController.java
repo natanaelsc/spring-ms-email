e60603824b304ef18b5fb6c74e26f68700209e46
natanaelsc <natanael96@live.com>
2023-02-06 11:53:20 -0300

    move EmailController to controllers

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -1,4 +1,4 @@
-package com.ms.email;
+package com.ms.email.controllers;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 
 import jakarta.validation.Valid;
 
