98cff06480bc749a374911d08d4c357b123354e7
natanaelsc <natanael96@live.com>
2023-02-06 11:55:07 -0300

    move EmailService to services

--- src/main/java/com/ms/email/services/EmailService.java
+++ src/main/java/com/ms/email/services/EmailService.java
@@ -1,4 +1,4 @@
-package com.ms.email;
+package com.ms.email.services;
 
 import java.time.LocalDateTime;
 
@@ -7,6 +7,10 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
 
 @Service
 public class EmailService {
