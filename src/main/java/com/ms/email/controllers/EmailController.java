d8d666762d8c775be467eec6d54051a0bd1fade1
natanaelsc <natanael96@live.com>
2023-02-06 14:13:17 -0300

    create getAllEmail and change email endpoint name to emails

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -2,8 +2,13 @@ package com.ms.email.controllers;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +23,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("api/v1/emails")
 public class EmailController {
-    
+
     @Autowired
     public EmailService emailService;
 
@@ -28,5 +33,11 @@ public class EmailController {
         BeanUtils.copyProperties(emailDto, emailModel);
         emailService.sendEmail(emailModel);
         return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EmailModel>> getAllEmails(
+            @PageableDefault(page = 0, size = 5, sort = "sendDateEmail", direction = Sort.Direction.DESC) Pageable pageable) {
+        return new ResponseEntity<>(emailService.findAll(pageable), HttpStatus.OK);
     }
 }
