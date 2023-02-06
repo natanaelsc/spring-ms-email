8f99f61f409da55cb7c0577548681693b2003cd1
natanaelsc <natanael96@live.com>
2023-02-06 14:50:13 -0300

    create getEmailById method

--- src/main/java/com/ms/email/controllers/EmailController.java
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -1,5 +1,8 @@
 package com.ms.email.controllers;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,6 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +43,15 @@ public class EmailController {
     public ResponseEntity<Page<EmailModel>> getAllEmails(
             @PageableDefault(page = 0, size = 5, sort = "sendDateEmail", direction = Sort.Direction.DESC) Pageable pageable) {
         return new ResponseEntity<>(emailService.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/{emailId}")
+    public ResponseEntity<Object> getOneEmail(@PathVariable UUID emailId) {
+        Optional<EmailModel> emailModelOptional = emailService.findById(emailId);
+        if (!emailModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found.");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(emailModelOptional.get());
+        }
     }
 }
